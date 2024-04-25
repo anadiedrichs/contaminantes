72bc14e34e8986b996866d34cf78f0fb93333196
Ana Laura Diedrichs <analauradiedrichs@gmail.com>
2024-04-25 12:25:19 -0300

    add timestamp column

--- EDA/cleaning.R
+++ EDA/cleaning.R
@@ -1,20 +1,23 @@
 library(readr)
-database <- read_delim("2024-04-24/database_2022_2023.csv", 
-                       delim = ";", escape_double = FALSE, col_types = cols(Dia = col_date(format = "%d/%m/%y"), 
-                                                                            `Dir viento (°N)` = col_skip(), 
-                                                                            `Dir viento` = col_skip()), locale = locale(date_names = "es", 
-                                                                                                                        decimal_mark = ",", grouping_mark = ";"), 
-                       trim_ws = TRUE)
+database_2022_2023 <- read_delim("2024-04-24/database_2022_2023.csv", 
+                                 delim = ";", escape_double = FALSE, 
+                                 col_types = cols(`Dir viento (°N)` = col_skip(), 
+                                  `Dir viento` = col_skip()), 
+                                 locale = locale(date_names = "es",                                                                                                                                   decimal_mark = ",", grouping_mark = ";"), 
+                                 trim_ws = TRUE)
 
-#View(database)
+#View(database_2022_2023)
+
 library(lubridate)
 
 # clean hour to correct format
-hora <- database$Hora
+hora <- database_2022_2023$Hora
 library(stringr)
 
 hora <- str_replace(hora,"a.*m.$","AM")
 hora <- str_replace(hora,"p.*m.$","PM")
-database$Hora <- hora
+var <- str_c(database_2022_2023$Dia, " ", hora)
 
 # create timestamp column from fecha and hour 
+database_2022_2023$time <- parse_date_time(var,"%d/%m/%y %H:%M %Op")
+
